0ed45713da2b3f72b4d8399da8673cc4b7c06464
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-18 21:26:41 -0500

    refactor checkDocumentExists

--- app/services/firestore.ts
+++ app/services/firestore.ts
@@ -1,8 +1,13 @@
 import { DocumentReference, getDoc, setDoc } from 'firebase/firestore';
 
 export const checkDocumentExists = async (docRef: DocumentReference): Promise<boolean> => {
+  try {
     const docSnapshot = await getDoc(docRef);
     return docSnapshot.exists();
+  } catch (error) {
+    console.error('Failed to check document existence.');
+    return false;
+  }
 };
 
 export const createUserDocument = async(docRef: DocumentReference, userData: any): Promise<void> => {
