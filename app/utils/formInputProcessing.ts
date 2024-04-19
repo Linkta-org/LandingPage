062f90b72f5dbcd62d5b36690a5f962b3602ac92
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-04-18 19:00:16 -0500

    simplify helper funcs

--- app/utils/formInputProcessing.ts
+++ app/utils/formInputProcessing.ts
@@ -1,29 +1,21 @@
 import { serverTimestamp } from "firebase/firestore";
 import { FormValues } from "../types/signupForm";
 
-export const cleanInput = (input: string | null | undefined): string => {
-    return input ? input.replace(/\s+/g, ' ').trim() : '';
-};
-
-export const parseCommaSeparatedInput = (input: string | null | undefined): string[] => {
-    return input ? input.split(',').map(item => item.trim()) : [];
-};
-
-export const cleanArray = (array: string[]): string[] => {
+export const removeEmptyItems = (array: string[]): string[] => {
     return array.length ? array.filter(item => item.length > 0) :[];
 };
 
 export const parseAndCleanInput = (input: string | null | undefined): string[] => {
-    const parsedInput = parseCommaSeparatedInput(input);
-    return cleanArray(parsedInput.map(item => cleanInput(item)));
+    const parsedInput = input ? input.split(',') : [];
+    return removeEmptyItems(parsedInput.map(item => item.trim()));
 };
 
 export const cleanUserData = (formData: FormValues) => {
   return {
-    name: cleanInput(formData.name),
-    email: cleanInput(formData.email),
+    name: formData.name.trim(),
+    email: formData.email,
     interests: parseAndCleanInput(formData.interests),
-    source: formData.source ? cleanInput(formData.source) : 'not provided',
+    source: formData.source ? formData.source.trim() : 'not provided',
     features: parseAndCleanInput(formData.features),
     createdAt: serverTimestamp(),
   };
