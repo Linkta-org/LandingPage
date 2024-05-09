af104ba68fe18aa4165dde97fba3a8a911284aaf
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-05-09 18:21:13 -0500

    refactor footer and removed links

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,22 +1,9 @@
-import { footerLinks } from './layoutConfig';
-import Link from 'next/link';
-
 export default function Footer() {
   return (
-    <>
-      <div className="text-center pt-2 sm:-mb-4 px-10 text-light-text">
-        <span className="justify-start flex-grow">
+    <div className="px-10 pt-2 text-center text-light-text sm:-mb-4">
+      <span className="flex-grow justify-start">
         ©2024 Linkta L.L.C. All rights reserved.
-        </span>
-     
-      <ul className="flex place-content-evenly hidden">
-        {footerLinks.map((tab) => (
-          <li key={`${tab.path}-${tab.tabname}`}>
-            <Link href={tab.path} className="px-1 flex-1">{tab.tabname}</Link>
-          </li>
-        ))}
-      </ul>
-      </div>
-    </>
+      </span>
+    </div>
   );
 }
