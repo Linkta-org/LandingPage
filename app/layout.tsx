12f28ad894ed8d2d6e7e77156f1ca4e359c4aad2
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-05-09 21:42:12 -0500

    update e2e tests

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import Footer from './components/layout/Footer';
-import LinktaLogoWithText from './components/layout/LinktaLogoWithText';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 
 export const metadata: Metadata = {
