9b8cdeeba47da9f1b0acc6ee94dadd4d21597a1c
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-05-09 21:25:36 -0500

    remove privacy agreement component

--- app/components/common/PrivacyAgreement.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function PrivacyAgreement() {
-  return (
-    <div className="text-xs mt-2">
-      By continuing you are agreeing to our <Link href="/privacy-policy">Privacy Policy</Link>
-    </div>
-  );
-}
