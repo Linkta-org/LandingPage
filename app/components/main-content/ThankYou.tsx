2352fdbf2f8979fafe403e210992cd3466cd9c6b
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-06-25 17:18:14 -0500

    updated thankyou

--- app/components/main-content/ThankYou.tsx
+++ app/components/main-content/ThankYou.tsx
@@ -7,43 +7,38 @@ const ThankYou = () => {
   const linktaEmail = 'info@linkta.org';
 
   return (
-    <article aria-live="polite" className="py-12 ml-4 mb-2 px-2 ">
-      <h2 className="flex pt-4 h-[60px] align-bottom">
-        <span className="text-3xl font-bold align-text-bottom">
-          Thank you!
-        </span>
+    <article
+      aria-live="polite"
+      className="mb-2 ml-4 px-2 py-12"
+    >
+      <h2 className="flex h-[60px] pt-4 align-bottom align-text-bottom text-3xl font-bold">
+        Thank you!
       </h2>
-      <section>
-        <p className="pt-4 text-lg">
-          We&apos;re thrilled to have you join us on this exciting journey!
-        </p>
-      </section>
-      <div>
-        <ClientSideLottie
-          src={mailAnimationData}
-          autoplay={true}
-          loop={true}
-          className="w-[300px] h-[200px]"
-        />
-      </div>
-      <section>
-        <p>
-          If you&apos;d like to keep updated with the project,{' '}
-          <a
-            href="https://www.linkedin.com/company/100947448/"
-            className="underline decoration-light-link font-semibold"
-          >
-            follow us on LinkedIn!
-          </a>{' '}
-          or email us at{' '}
-          <a
-            href={`mailto:${linktaEmail}`}
-            className="underline decoration-light-link font-semibold"
-          >
-            {linktaEmail}
-          </a>
-        </p>
-      </section>
+      <p className="pt-4 text-lg">
+        We're excited to have you with us on this journey!
+      </p>
+      <ClientSideLottie
+        src={mailAnimationData}
+        autoplay={true}
+        loop={true}
+        className="h-[200px] w-[300px]"
+      />
+      <p className="pt-4 text-base">
+        Stay updated with our progress by following us on{' '}
+        <a
+          href="https://www.linkedin.com/company/100947448/"
+          className="font-semibold underline decoration-light-link"
+        >
+          LinkedIn
+        </a>{' '}
+        or reach out to us at{' '}
+        <a
+          href={`mailto:${linktaEmail}`}
+          className="font-semibold underline decoration-light-link"
+        >
+          {linktaEmail}
+        </a>
+      </p>
     </article>
   );
 };
