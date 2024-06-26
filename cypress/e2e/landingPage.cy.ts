9fbab990999dbe90dcbdf06fdc00dbb7ca7d3b0a
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-06-26 11:12:58 -0500

    commented out cy

--- cypress/e2e/landingPage.cy.ts
+++ cypress/e2e/landingPage.cy.ts
@@ -1,28 +1,28 @@
-import LandingPage from '../support/pageObjects/LandingPage';
-import LandingPageValidator from '../support/validators/LandingPageValidator';
+// import LandingPage from '../support/pageObjects/LandingPage';
+// import LandingPageValidator from '../support/validators/LandingPageValidator';
 
-describe('Landing Page', () => {
-  let landingPage: LandingPage;
-  let landingPageValidator: LandingPageValidator;
+// describe('Landing Page', () => {
+//   let landingPage: LandingPage;
+//   let landingPageValidator: LandingPageValidator;
 
-  beforeEach(() => {
-    landingPage = new LandingPage();
-    landingPageValidator = new LandingPageValidator(landingPage);
-    landingPage.open();
-  });
+//   beforeEach(() => {
+//     landingPage = new LandingPage();
+//     landingPageValidator = new LandingPageValidator(landingPage);
+//     landingPage.open();
+//   });
 
-  xit('should validate if name input contains special characters', () => {
-    landingPage.setName('Fake Name!@#');
-    landingPageValidator.expectErrorNameContainsSpecialCharacters();
-  });
+//   it('should validate if name input contains special characters', () => {
+//     landingPage.setName('Fake Name!@#');
+//     landingPageValidator.expectErrorNameContainsSpecialCharacters();
+//   });
 
-  xit('should validate if email input is incorrect', () => {
-    landingPage.setEmail('fake@email');
-    landingPageValidator.expectErrorEmailAddressIncorrect();
-  });
+//   it('should validate if email input is incorrect', () => {
+//     landingPage.setEmail('fake@email');
+//     landingPageValidator.expectErrorEmailAddressIncorrect();
+//   });
 
-  xit('should validate if email input is too short', () => {
-    landingPage.setEmail('abc');
-    landingPageValidator.expectShortEmailErrorMessage();
-  });
-});
+//   it('should validate if email input is too short', () => {
+//     landingPage.setEmail('abc');
+//     landingPageValidator.expectShortEmailErrorMessage();
+//   });
+// });
