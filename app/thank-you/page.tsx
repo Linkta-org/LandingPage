ef5e9ba1dc0c15d6c3a6ea2d099f5339e62ef7b4
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-06-25 20:39:58 -0500

    minor update

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,16 +1,13 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { isSignInWithEmailLink } from 'firebase/auth';
 import { auth } from '../config/firebase';
 import { authenticateAndSaveUserDataFromEmailRedirect } from '../services/emailAuth';
 import { useRouter } from 'next/navigation';
-import Loading from '../loading';
 import ThankYou from '../components/main-content/ThankYou';
 
 export default function ThankYouPage() {
-  const [isLoading, setIsLoading] = useState(true);
-
   const router = useRouter();
   useEffect(() => {
     if (!isSignInWithEmailLink(auth, window.location.href)) {
@@ -19,19 +16,14 @@ export default function ThankYouPage() {
       let email = window.localStorage.getItem('emailForSignIn');
       if (email) {
         authenticateAndSaveUserDataFromEmailRedirect(email);
-        setIsLoading(false);
       } else {
         router.push('/');
       }
     }
   }, [router]);
 
-  if (isLoading) {
-    return <Loading />;
-  }
-
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center text-light-text">
       <section
         id="thank-you"
         className="mx-auto my-4 flex w-full flex-col items-center justify-center p-4"
